41f41dea59b7dfece43ed380a93ab470c3c9abc8
Nick Youngblut <nyoungb2@illinois.edu>
2013-02-02 15:32:29 -0600

    extract-part: added mcl calling

--- bin/extract-partitions-paired.pl
+++ bin/extract-partitions-paired.pl
@@ -8,8 +8,7 @@ use Data::Dumper;
 use Getopt::Long;
 use File::Spec;
 use File::Path qw/remove_tree/;
-use Graph;
-use Graph::Undirected;
+use IPC::Open2;
 
 ### args/flags
 pod2usage("$0: No files given.") if ((@ARGV == 0) && (-t STDIN));
@@ -18,10 +17,14 @@ my ($verbose);
 my $max_size = 1000000;
 my $min_part_size = 10;
 my $status_int = 1000000;
+my $inflation = 2;				# inflation param default
+my $threads = 1;
 GetOptions(
 	   "max-size=i" => \$max_size,						# max group size
 	   "min-partition-size=i" => \$min_part_size,		# min partition size worth keeping
 	   "status=i" => \$status_int,						# how often to provide a status update
+	   "inflation=f" => \$inflation,					# mcl inflation param
+	   "threads=i" => \$threads,						# number of threads for mcl
 	   "verbose" => \$verbose,
 	   "help|?" => \&pod2usage # Help
 	   );
@@ -30,101 +33,27 @@ GetOptions(
 die " ERROR: provide a *part file\n" if ! $ARGV[0];
 
 ### MAIN
-my $basename = sort_pairs($ARGV[0]);
-
-### making graph ###
-my $g = make_paired_graph($basename);
-merge_connected($g, $basename);
-
-### writing out Moth files ###
-#my ($connect_r, $max_connections, $names_r) = paired_connectivity($basename);
-#write_names($basename, $names_r);
-#$connect_r = normalize_connections($connect_r, $max_connections);
-#write_connect_table($basename, $connect_r);
-exit;
+my $basename = sort_pairs($ARGV[0]);										# parsing out pairs
+my $connect_r = partition_connection_hash($basename);						# making a parition graph for loading into mcl
+my $mcl_out = call_mcl($connect_r, $basename, $inflation, $threads);		# clustering w/ mcl
+my $clusters_r = load_mcl_out($mcl_out);
+my $groups = parts_in_groups($clusters_r, $max_size, $min_part_size)		# placing paritions into groups
+write_groups($clusters_r, $basename);
 
 ### writing out groups ###
-my $parts_r = count_parts($basename);
-my $groups_r = parts_in_groups($parts_r, $max_size, $min_part_size);
-write_groups($groups_r, $basename);
+#my $parts_r = count_parts($basename);
+#my $groups_r = parts_in_groups($parts_r, $max_size, $min_part_size);
+#write_groups($groups_r, $basename);
 
 ### Subroutines
-sub merge_connected{
-# merging pairtitions connected by paired-end reads #
-	my ($g, $basename) = @_;
-
-	print STDERR " Finding connected components\n";
-	my @cc = $g->connected_components();
-	
-	# status #
-	print STDERR " Number of groups (connected partitions): ", scalar @cc, "\n";
-	
-	# component size distribution #
-	print STDERR " Writting size distribution\n";
-	my %size_dist;
-	map{ $size_dist{scalar @$_}++ } @cc;
-	print "\n", join("\t", qw/N_partitions N_groups/), "\n";
-	foreach my $size (sort {$a <=> $b} keys %size_dist){
-		print join("\t", $size, $size_dist{$size}), "\n";
-		}
-	print "\n"; 
-	
-	}
-
-sub make_paired_graph{
-# merging partitions based on paired-end reads #
-	my ($basename) = @_;
-	
-	# status #
-	print STDERR " Making graph\n";
-	
-	# reading pair file and making pair hash #
-	open PAIR, "$basename-pair.fna" or die $!;
-
-	# making graph object #
-	my $g = Graph::Undirected->new;
-
-	my $max_connections = 0;		# for normalizing connections
-	while(<PAIR>){
-		chomp;
-		# status #
-		if(($. -1) % $status_int == 0){
-			print STDERR " lines processed: ", ($. - 1) / 2, "\n";
-			}
-	
-		# loading lines #
-		my @line1 = split /\t/;
-		my $line2 = <PAIR>;
-		my @line3 = split /\t|\n/, <PAIR>;
-		my $line4 = <PAIR>;
-		
-		# adding to merge hash #
-		if($line1[1] ne $line3[1]){		# if partitions for pairs don't match, add to graph
-			$g->add_edge($line1[1], $line3[1]);
-			}
-			
-		}
-	close PAIR;
-
-		#print "The graph is $g\n";
-	# graph stats #
-	print STDERR " The graph has ", scalar $g->vertices, " vertices & ", scalar $g->edges, " edges\n";	
-	
-	return $g;
-	}
-
-
-
-
 sub write_groups{
 # writing out partitions in groups #
-	my ($groups_r, $basename) = @_;
-	
-	# getting unique groups #
-	my %ugroups = map{$_, 1} values %$groups_r; 
+	my ($clusters_r, $basename) = @_;
 	
 	# status #
-	print STDERR " Number of groups: ", scalar keys %ugroups, "\n";
+	print STDERR " Writing out group files\n";
+	
+	
 
 	# output dir #
 	my $dirname = $basename . "_groups";
@@ -182,7 +111,6 @@ sub write_groups{
 	close SING;
 	map{ close $fh{$_} } keys %fh;
 	
-	
 	}
 
 sub parts_in_groups{
@@ -207,176 +135,70 @@ sub parts_in_groups{
 	return \%groups;
 	}
 
-sub count_parts{
-### counting partitions in each file ###
-	my ($basename) = @_;
 
+sub load_mcl_out{
+# loading mcl output into a hash #
+	my ($mcl_out) = @_;
+	
 	# status #
-	print STDERR " Counting partitions\n";
-
-	# counting pairs #	
-	open PAIR, "$basename-pair.fna" or die $!;
-
-	my %parts;
-	while(<PAIR>){
-		
-		# status #
-		if(($. -1) % $status_int == 0){
-			print STDERR " lines processed: ", ($. - 1) / 2, "\n";
-			}
-		
-		my @line = split /\t/;
-		$parts{$line[1]} += 2;
-		for my $i (0..2){		# skipping 2nd in pair
-			my $line = <PAIR>;
-			}
-		}
-	close PAIR;
+	print STDERR " Loading mcl output\n";
 	
-	# counting singles #
-	open SING, "$basename-single.fna" or die $!;
-	while(<SING>){
-		
-		# status #
-		if(($. -1) % $status_int == 0){
-			print STDERR " lines processed: ", ($. - 1) / 2, "\n";
-			}
-		
+	open IN, $mcl_out or die $!;
+	my %clusters;
+	while(<IN>){
+		chomp;
 		my @line = split /\t/;
-		my $nline = <SING>;
-		$parts{$line[1]}++;
+		map{ $clusters{$_} = $. } @line;			# all partitions in cluster pointing to clsuter ID
 		}
-	close SING;
-
-
-	return \%parts;	
+	close IN;
+	
+		#print Dumper %clusters; exit;
+	return \%clusters;
 	}
 
-
-
-
-
-sub merge_merges{
-# reciprically merging hashes until no more linkages found #
-	my ($merge_hash_r) = @_;
-
+sub call_mcl{
+# calling mcl for clustering #
+	my ($connect_r, $basename, $inflation, $threads) = @_;
+	
 	# status #
-	print STDERR " Merging partitions in merge hash\n";
+	print STDERR " Calling mcl\n";
 	
+	# making an output file name #
+	my $mcl_out = $basename . "_mcl.txt";
 	
-	my $N_group = scalar keys %$merge_hash_r;		# saving last group size (1-step memory)
+	my $cmd = "mcl - --abc -I $inflation -te $threads -o $mcl_out";
+	print STDERR $cmd, "\n";
+	open PIPE, " | $cmd" or die $!;
 	
-	$merge_hash_r = merge_recip($merge_hash_r, $N_group);
-	
-	print Dumper $merge_hash_r; exit;
-	
-	## sub ##
-	sub merge_recip{
-	# reciprical merging #
-		my ($merge_hash_r, $N_group) = @_;
-	
-		
-		# merging if hash-lev1 value == hash-lev2 value (within any lev1 hash) #
-		foreach my $query (keys %$merge_hash_r){		# checking other hashes for $merge
-			my @to_merge;
-			foreach my $merge (keys %$merge_hash_r){				# going through each merge-group and checking for query
-				if( exists  $$merge_hash_r{$merge}{$query} ){		# could apply count number required HERE
-					push(@to_merge, $merge); 						# list of merge-groups to merge
-					next;
-					}
-				}
-			
-			if(@to_merge){
-				print Dumper scalar @to_merge; 
-				}
-			
-			# merging all merge-groups into the query group #
-			foreach my $merging (@to_merge){
-				foreach my $part (keys %{$$merge_hash_r{$merging}}){		# adding to the other group
-					$$merge_hash_r{$query}{$part} = $$merge_hash_r{$merging}{$part};
-					}
-				delete $$merge_hash_r{$merging}; 		# removing the original group
-				}
-			}
-			
-		# checking number of groups after merging #
-		my $N2_group = scalar keys %$merge_hash_r;
-		
-		# status #
-		print STDERR " B4_merge: $N_group; After_merge: $N2_group\n";
-		
-		# running again if less groups than before #
-		if($N2_group < $N_group){
-			merge_recip($merge_hash_r, $N2_group);
-			}
-		else{
-			return $merge_hash_r;
+	# running mcl #
+	foreach my $c1 (keys %$connect_r){
+		foreach my $c2 (keys %{$$connect_r{$c1}}){
+			print PIPE join(" ", $c1, $c2, $$connect_r{$c1}{$c2}), "\n";
 			}
 		}
-	
+	close PIPE;
+
+	return $mcl_out;
 	}
 
-sub write_connect_table{
-# writing connectivity table (paired-ends spanning partitions) #
-	my ($basename, $connect_r) = @_;
-	
-	open OUT, ">$basename-pairnet.dist" or die $!;
-	
-	foreach my $part1 (keys %$connect_r){
-		foreach my $part2 (keys %{$$connect_r{$part1}}){
-			print OUT join("\t", $part1, $part2, $$connect_r{$part1}{$part2}), "\n";
-			}
-		}
-	close OUT;
-
-	}
-
-sub normalize_connections{
-#  normalizing connections by max connection #
-	my ($connect_r, $max_connections) = @_;
-	
-	foreach my $part1 (keys %$connect_r){
-		foreach my $part2 (keys %{$$connect_r{$part1}}){
-			$$connect_r{$part1}{$part2} = $$connect_r{$part1}{$part2} / $max_connections;
-			}
-		}
-
-		#print Dumper $connect_r; exit;
-	return $connect_r; 
-	}
-
-sub write_names{
-# writing names file #
-	my ($basename, $names_r) = @_;
-	
-	open OUT, ">$basename.names" or die $!;
-	
-	foreach (keys %$names_r){
-		print OUT join("\t", $_, $_), "\n";
-		}
-	
-	close OUT;
-	}
-
-sub paired_connectivity{
+sub partition_connection_hash{
 # merging partitions based on paired-end reads #
 	my ($basename) = @_;
 	
 	# status #
-	print STDERR " Making merge hash\n";
+	print STDERR " Making connectedness hash\n";
 	
 	# reading pair file and making pair hash #
 	open PAIR, "$basename-pair.fna" or die $!;
 
-	
+	# making connectedness hash #
+	#my $max_connections = 0;		# for normalizing connections
 	my %connect;
-	my %names;
-	my $max_connections = 0;		# for normalizing connections
 	while(<PAIR>){
 		chomp;
 		# status #
 		if(($. -1) % $status_int == 0){
-			print STDERR " lines processed: ", ($. - 1) / 2, "\n";
+			print STDERR " Read pairs processed: ", ($. - 1) / 2, "\n";
 			}
 	
 		# loading lines #
@@ -386,24 +208,19 @@ sub paired_connectivity{
 		my $line4 = <PAIR>;
 		
 		# adding to merge hash #
-		if($line1[1] ne $line3[1]){		# comparing partitions for pairs and adding to partition merge
-			$connect{$line1[1]}{$line3[1]}++;
-			$max_connections = $connect{$line1[1]}{$line3[1]} if $connect{$line1[1]}{$line3[1]} > $max_connections;
-			}
+		$connect{$line1[1]}{$line3[1]}++ if $line1[1] ne $line3[1];
 			
-		# adding to %names  for making a names file #
-		$names{$line1[1]} = 1;
-		$names{$line3[1]} = 1;
 		}
 	close PAIR;
 
-		#print Dumper $max_connections; exit;	
 		#print Dumper %connect; exit;
-	return \%connect, $max_connections, \%names;
+
+	return \%connect;
 	}
 
+
 sub sort_pairs{
-# counting number of reads in each partition #
+# parsing out existing paired-end reads #
 	my ($infile) = @_;
 	
 	# status #
@@ -427,6 +244,8 @@ sub sort_pairs{
 		
 		# load lines #
 		my @line = split /\//;		# header, pair, partition
+		die " ERROR: read names must be in old illumina format (>NAME/1 or >NAME/2)\n"
+			if scalar @line != 2;
 		my $nline = <IN>;
 
 		# checking for pairs; writing files #
@@ -451,50 +270,6 @@ sub sort_pairs{
 	return $basename;
 	}
 
-
-
-sub find_pairs_and_write{
-	my $infile = shift;
-	
-	# I/O #
-	(my $basename = $infile) =~ s/\.[^\.]+$|$//;
-	
-	remove_tree(".$basename") if -d ".$basename";
-	mkdir ".$basename"; 
-	
-	open IN, $infile or die $!;
-	
-	# running through file #
-	my %seq_names;
-	my %parts;
-	while(<IN>){
-		my @line = split /\t|\//;		# header, pair, partition
-		
-		if(! exists($parts{$line[2]})){		# if need to make a file
-			open $parts{$line[2]}, ">.$basename/$line[2].part" or die $!;
-			
-			# writing read #
-			print {$parts{$line[2]}} $_;
-			my $nline = <IN>;
-			print {$parts{$line[2]}} $nline;
-			}
-		else{
-			print {$parts{$line[2]}} $_;
-			my $nline = <IN>;
-			print {$parts{$line[2]}} $nline;
-			}
-		
-		# checking for other pair #
-		#if(exists $seq_names{$line[1]} ){		# if paired file found 		
-		#	}
-		
-		
-		}
-	
-	# closing #
-	close IN;
-	map{ close $parts{$_} } keys %parts; 
-	}
 
 
 __END__
